d779e4c7e5aeea8e48ba0fcf332bc4f3947d4f1b
Egor Kondratev <kondratevegor04@gmail.com>
2024-10-03 20:24:21 +0300

    fix: siren decoration

--- src/features/operator/SirenDecoration.tsx
+++ src/features/operator/SirenDecoration.tsx
@@ -1,9 +1,9 @@
 const SirenDecoration = () => {
   return (
     <>
-      <div className="absolute right-[-100px] top-1/4 size-[300px] rounded-full bg-blue-600 blur-[200px] animate-pulse" />
+      <div className="absolute right-[-150px] top-1/5 size-[400px] rounded-full bg-blue-600 blur-[200px] animate-pulse" />
 
-      <div className="absolute right-[-100px] top-2/4 size-[300px] rounded-full bg-red-600 blur-[200px] animate-pulse delay-1000" />
+      <div className="absolute right-[-150px] top-2/4 size-[400px] rounded-full bg-red-600 blur-[200px] animate-pulse delay-1000" />
     </>
   );
 };
